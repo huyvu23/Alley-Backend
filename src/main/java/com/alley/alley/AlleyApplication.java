a5f35a5c0f56598f144fd8fcf23ee585192ff674
Dominik <93518371+huyvu23@users.noreply.github.com>
2026-03-16 16:55:41 +0700

    first commit

---
+++ src/main/java/com/alley/alley/AlleyApplication.java
@@ -0,0 +1,13 @@
+package com.alley.alley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alley/alley/AlleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alley.alley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
